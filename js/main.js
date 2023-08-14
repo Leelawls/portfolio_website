1b4cd1d2611743bf5177495f6ba8c44f740754ff
LeeLawl <laulishuo@gmail.com>
2023-08-14 09:34:40 -0500

    about me changes

--- js/main.js
+++ js/main.js
@@ -25,10 +25,9 @@
 
         });
 
-        // force page scroll position to top at page refresh
-        // window.addEventListener('beforeunload' , function () {
-        //     window.scrollTo(0, 0);
-        // });
+        window.addEventListener('beforeunload' , function () {
+            window.scrollTo(0, 0);
+        });
 
     }; // end ssPreloader
 
